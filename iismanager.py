c13d717576bcf984e2c977954537d4a6c4d15422
karonis <kostas@DESKTOP-VDAR2GB.localdomain>
2020-06-02 18:08:10 +0300

    fixes

--- iismanager.py
+++ iismanager.py
@@ -1,4 +1,5 @@
 import requests
+from requests_ntlm import HttpNtlmAuth
 import sys
 
 
@@ -11,10 +12,11 @@ class IisManager:
     started = {"status": "started"}
     recycling = {"status": "recycling"}
 
-    def __init__(self, uri, user, pwd):
+    def __init__(self, uri, user, pwd, token):
         self.uri = uri
         self.user = user
         self.pwd = pwd
+        self.token = token
 
     def getHeaders(self):
         return {'Content-Type': 'application/json', 'Accept': 'application/hal+json', 'Access-Token': 'Bearer '+self.token}
@@ -23,7 +25,10 @@ class IisManager:
         return self.uri + part
 
     def getServices(self):
-        return requests.get(self.getUrl("api/webserver/websites"), , headers=self.getHeaders(), verify=False).json()
+        res = requests.get(self.getUrl("api/webserver/websites"), headers=self.getHeaders(), verify=False, auth=HttpNtlmAuth(self.user,self.pwd))
+        print(res.status_code)
+        print(res.json())
+        return res.json()['websites']
 
     def getServiceId(self, name):
         services = self.getServices()
@@ -33,26 +38,39 @@ class IisManager:
 
     def stopService(self, name):
         id = self.getServiceId(name)
-        requests.patch(self.getUrl(
-            "api/webserver/websites/"+id), json=self.stopped, headers=self.getHeaders(), verify=False)
+        res = requests.patch(self.getUrl(
+            "api/webserver/websites/"+id), json=self.stopped, headers=self.getHeaders(), verify=False, auth=HttpNtlmAuth(self.user,self.pwd))
 
     def startService(self, name):
         id = self.getServiceId(name)
         requests.patch(self.getUrl(
-            "api/webserver/websites/"+id), json=self.started, headers=self.getHeaders(), verify=False)
+            "api/webserver/websites/"+id), json=self.started, headers=self.getHeaders(), verify=False, auth=HttpNtlmAuth(self.user,self.pwd))
 
     def getAppPools(self):
-        return requests.get(self.getUrl("api/webserver/application-pools"), headers=self.getHeaders(), verify=False).json()
+        res = requests.get(self.getUrl("api/webserver/application-pools"), headers=self.getHeaders(), verify=False, auth=HttpNtlmAuth(self.user,self.pwd))
+        print(res.json())
+        return res.json()['app_pools']
+
+    def getAppPoolId(self, name):
+        services = self.getAppPools()
+        for s in services:
+            if s['name'] == name:
+                return s['id']
 
     def stopAppPool(self, name):
-        id = self.getAppPools()
+        id = self.getAppPoolId(name)
         requests.patch(self.getUrl(
-            "api/webserver/application-pools/"+id), json=self.stopped, headers=self.getHeaders(), verify=False)
+            "api/webserver/application-pools/"+id), json=self.stopped, headers=self.getHeaders(), verify=False, auth=HttpNtlmAuth(self.user,self.pwd))
 
     def startAppPool(self, name):
-        id = self.getAppPools()
+        id = self.getAppPoolId(name)
         requests.patch(self.getUrl(
-            "api/webserver/application-pools/"+id), json=self.started, headers=self.getHeaders(), verify=False)
+            "api/webserver/application-pools/"+id), json=self.started, headers=self.getHeaders(), verify=False, auth=HttpNtlmAuth(self.user,self.pwd))
+
+    def recycleAppPool(self, name):
+        id = self.getAppPoolId(name)
+        requests.patch(self.getUrl(
+            "api/webserver/application-pools/"+id), json=self.recycling, headers=self.getHeaders(), verify=False, auth=HttpNtlmAuth(self.user,self.pwd))
 
 
 def main(argv):
@@ -64,7 +82,8 @@ def main(argv):
     pwd = argv[2]
     token = argv[3]
     name = argv[4]
-    manager = IisManager(uri, user, pwd)
+    print(argv)
+    manager = IisManager(uri, user, pwd, token)
 
     for action in argv[5:]:
         func = getattr(manager, action)
