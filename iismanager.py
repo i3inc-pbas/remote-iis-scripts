dfb9037c275b65665316569461881d00a96a18b6
Portno <j.remoundos@dotbydot.gr>
2020-06-02 16:27:11 +0300

    Create iismanager.py

---
+++ iismanager.py
@@ -0,0 +1,74 @@
+import requests
+import sys
+
+
+class IisManager:
+    uri = ""
+    user = ""
+    pwd = ""
+    token = ""
+    stopped = {"status": "stopped"}
+    started = {"status": "started"}
+    recycling = {"status": "recycling"}
+
+    def __init__(self, uri, user, pwd):
+        self.uri = uri
+        self.user = user
+        self.pwd = pwd
+
+    def getHeaders(self):
+        return {'Content-Type': 'application/json', 'Accept': 'application/hal+json', 'Access-Token': 'Bearer '+self.token}
+
+    def getUrl(self, part):
+        return self.uri + part
+
+    def getServices(self):
+        return requests.get(self.getUrl("api/webserver/websites"), self.getHeaders()).json()
+
+    def getServiceId(self, name):
+        services = self.getServices()
+        for s in services:
+            if s['name'] == name:
+                return s['id']
+
+    def stopService(self, name):
+        id = self.getServiceId(name)
+        requests.patch(self.getUrl(
+            "api/webserver/websites/"+id), json=self.stopped)
+
+    def startService(self, name):
+        id = self.getServiceId(name)
+        requests.patch(self.getUrl(
+            "api/webserver/websites/"+id), json=self.started)
+
+    def getAppPools(self):
+        return requests.get(self.getUrl("api/webserver/application-pools"), self.getHeaders()).json()
+
+    def stopAppPool(self, name):
+        id = self.getAppPools()
+        requests.patch(self.getUrl(
+            "api/webserver/application-pools/"+id), json=self.stopped)
+
+    def startAppPool(self, name):
+        id = self.getAppPools()
+        requests.patch(self.getUrl(
+            "api/webserver/application-pools/"+id), json=self.started)
+
+def main(argv):
+    if len(argv) <= 5:
+        print("Args")
+        exit(1)
+    uri = argv[0]
+    user = argv[1]
+    pwd = argv[2]
+    token = argv[3]
+    name = argv[4]
+    manager = IisManager(uri, user, pwd)
+
+    for action in argv[5:]:
+        func = getattr(manager, action)
+        func(name)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
